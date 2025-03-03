953d3ef390c39cf2aad8130f8fee1537415e09b2
Naman-Parlecha <namanparlecha@gmail.com>
2025-03-04 02:28:20 +0530

    feat: Auth Middleware

--- backend/internal/middleware/auth_middleware.go
+++ backend/internal/middleware/auth_middleware.go
@@ -1,5 +1,39 @@
 package middleware
 
-func main() {
+import (
+	"net/http"
+	"strings"
 
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
+)
+
+func AuthMiddleware(JWTSecret string) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		tokenString := ctx.GetHeader("Authorization")
+
+		if tokenString == "" {
+			ctx.JSON(http.StatusUnauthorized, gin.H{"message": "authorization token not found"})
+			ctx.Abort()
+			return
+		}
+
+		tokenString = strings.TrimPrefix(tokenString, "Bearer ")
+
+		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+			return []byte(JWTSecret), nil
+		})
+		if err != nil || !token.Valid {
+			ctx.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid token"})
+			ctx.Abort()
+			return
+		}
+
+		if claims, ok := token.Claims.(jwt.MapClaims); ok {
+			ctx.Set("userID", claims["sub"])
+			ctx.Set("userEmail", claims["email"])
+		}
+
+		ctx.Next()
+	}
 }
