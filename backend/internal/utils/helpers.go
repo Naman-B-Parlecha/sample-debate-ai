afaf604beb986b9a1206873e981e61a09fb448f8
Naman-B-Parlecha <namanparlecha@gmail.com>
2025-03-29 23:07:13 +0530

    feat: Elo Rating

--- backend/internal/utils/helpers.go
+++ backend/internal/utils/helpers.go
@@ -1,1 +1,17 @@
 package utils
+
+import "math"
+
+func CalculateNewEloRating(currentUserRating float32, currentOpponentRating float32, userScore float32, gamesPlayed int) float32 {
+	expectedRating := 1.0/1.0 - math.Pow(10, float64((currentUserRating-currentOpponentRating)/400.0))
+
+	k := 32
+	if gamesPlayed < 15 {
+		k = 16
+	}
+
+	changedRating := float32(k) * (userScore - float32(expectedRating))
+	newRating := currentUserRating + changedRating
+
+	return newRating
+}
